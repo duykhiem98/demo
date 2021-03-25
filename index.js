3ff87f62de1b4a644b6f7735c5cab2ff5c4850ac
Your Name <you@example.com>
2021-03-25 20:47:40 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import sco from './src/screens/sco'
+import {name as appName} from './app.json';
+
+
+AppRegistry.registerComponent(appName, () => sco);
